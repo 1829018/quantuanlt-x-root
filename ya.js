2d867d284e279e28098f9546ff6bd86e22feb354
Jay-Ruoput <98442673+1829018@users.noreply.github.com>
2022-11-06 20:27:48 +0800

    Create ya.js

---
+++ ya.js
@@ -0,0 +1,17 @@
+/******************************
+⚠️如果放远程，请把ya.js替换成运程链接⚠️
+  🧚🏻‍♂️作者：🍡魔法师、木木🍡
+   交流群：1077223830
+*******************************
+
+[rewrite_local]
+https:\/\/cat-match-static\.easygame2021\.com\/datas\/gd_skin_list\.json script-response-body ya.js
+
+
+[mitm]
+hostname = cat-match-static.easygame2021.com
+
+*******************************/
+var body=$response.body;
+body = body.replace(/clothesId":\d+/g,'clothesId": 1');
+$done(body);
