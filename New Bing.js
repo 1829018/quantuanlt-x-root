92c951e1feff7b9f82a850f6049204e5a06b5723
Jay-Ruoput <98442673+1829018@users.noreply.github.com>
2023-06-09 21:13:19 +0800

    Update New Bing.js

--- New Bing.js
+++ New Bing.js
@@ -1,3 +1,10 @@
 hostname = *.bing.com
 
-(?<=cc=)CN(?=&) url 302 US
+var url = $request.url;
+if (url === "https://www.bing.com/sydchat?iOS=1&cc=CN&setlang=zh-hans&retry=3&auth=1") {
+    var redirectUrl = "https://www.bing.com/rewardsapp/reportActivity?cc=US&auth=1&iOS=1&setlang=zh-hans";
+    $done({ response: { status: 302, headers: { Location: redirectUrl } } });
+} else {
+    $done({});
+}
+
